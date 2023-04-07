09be82054fc28e9c4e0423c9d93234cb9cf13c21
die <119799764+ddies@users.noreply.github.com>
2023-04-07 13:01:53 +0200

    Update tos.py

--- bot/tos.py
+++ bot/tos.py
@@ -2,17 +2,3 @@ import  resources.tos as week_TOS
 
 
 class week(week_TOS):
-
-    def __init__(self):
-        self.username = "vqea"
-        self.description = "coding since 1/26/23, don't forget to follow and star for more :)"
-        self.contacts = {
-            "discord": "slya#4184",
-            "tiktok": ["@slya", "@dqae"]
-        }
-        self.aliases = [
-            "vqea",
-            "dqae",
-            "slya",
-            "lv"
-        ]
