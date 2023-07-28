568315ee58bbcb40d48e12105cb63ebe220e2fc7
die <119799764+ddies@users.noreply.github.com>
2023-07-28 17:07:41 +0200

    Update privacy_policy.py

--- bot/privacy_policy.py
+++ bot/privacy_policy.py
@@ -2,13 +2,13 @@ import  resources.privacy as week_privacypolicy
 
 import resources.server as gulid
 
-class week(week_privacypolicy):
+class use(use_privacypolicy):
 
     def __init__(week):
 
-        week.privacy = "Privacy Policy for weekbot"
-        week.malicious = "Any information we collect is not used maliciously. If any information stated here seems/is misleading, please contact us immediately at info@viliebot.com."
-        week.stores = [
+        use.privacy = "Privacy Policy for weekbot"
+        use.malicious = "Any information we collect is not used maliciously. If any information stated here seems/is misleading, please contact us immediately at info@viliebot.com."
+        use.stores = [
             "gulid IDs",
             "gulid Names",
             "channel IDs",
@@ -19,16 +19,16 @@ class week(week_privacypolicy):
             "last deleted message content",
             "last message edit history"
         ]
-        week.why_do_we_need_date_and_how_we_use_it = {
+        use.why_do_we_need_date_and_how_we_use_it = {
             "snipe": "when a command is unvoked, we store that message content forever for debugging purposes. for edited messaged and sniping messages",
             "discordContent": "gulid IDs, gulid Names, channel IDs, channel Names, message IDs  are all stored in our system safe",
             "usernames": "username changes are logged in order for the 'usernames' command to function. Users can clear this data themselves at any time"
         }
-        week.data_collection = [
+        use.data_collection = [
             "we do not sell and expose your information to third parties by any means"
         ]
-        week.data_removal = [
+        use.data_removal = [
             "email info@viliebot.com to have your data deleted. note that when emailing, please be specific with that information that you want gone and provide ownership of your Discord account. response time may vary and could take up to two weeks to processed"
         ]
-        week.warn = "we can update these terms at any time without notice. continuing to use our services after any changes will mean that you agree with these terms and violation of our terms of service could result in a permanent ban across all of our services"
+        use.warn = "we can update these terms at any time without notice. continuing to use our services after any changes will mean that you agree with these terms and violation of our terms of service could result in a permanent ban across all of our services"
         
